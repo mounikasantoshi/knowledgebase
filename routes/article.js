06cc83d6e664acecf0fc9c38d39c06bb22bd8e9d
mounika <mounikasantoshi99@gail.com>
2020-06-11 23:08:31 +0530

    initial commit

---
+++ routes/article.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const router = express.Router();
+
+//Bring in Article models
+let Article = require('../models/article');
+
+//Add route
+router.get('/add', function (req, res) {
+  res.render('add_article', {
+    title: 'Add Article',
+  });
+});
+
+//add submit POST Route
+router.post('/add', function (req, res) {
+  req.checkBody('title', 'Title is require').notEmpty();
+  req.checkBody('author', 'Author is require').notEmpty();
+  req.checkBody('body', 'Body is require').notEmpty();
+
+  //get Errors
+  let errors = req.validationErrors();
+
+  if (errors) {
+    res.render('add_article', {
+      title: 'Add Article',
+      errors: errors,
+    });
+  } else {
+    let article = new Article();
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+
+    article.save(function (err) {
+      if (err) {
+        console.log(err);
+        return;
+      } else {
+        req.flash('success', 'Article Added');
+        res.redirect('/');
+      }
+    });
+  }
+});
+
+//load edit form
+router.get('/edit/:id', function (req, res) {
+  Article.findById(req.params.id, function (err, article) {
+    res.render('edit_article', {
+      title: 'Edit Article',
+      article: article,
+    });
+  });
+});
+
+//Update submit POST Route
+router.post('/edit/:id', function (req, res) {
+  let article = {};
+  article.title = req.body.title;
+  article.author = req.body.author;
+  article.body = req.body.body;
+
+  let query = { _id: req.params.id };
+
+  Article.update(query, article, function (err) {
+    if (err) {
+      console.log(err);
+      return;
+    } else {
+      req.flash('success', 'Article updated');
+      res.redirect('/');
+    }
+  });
+});
+
+//Delete Article
+router.delete('/:id', function (req, res) {
+  let query = { _id: req.params.id };
+
+  Article.remove(query, function (err) {
+    if (err) {
+      console.log(err);
+    }
+    res.send('success');
+  });
+});
+
+//get single article
+router.get('/:id', function (req, res) {
+  Article.findById(req.params.id, function (err, article) {
+    res.render('article', {
+      article: article,
+    });
+  });
+});
+
+module.exports = router;
